9970a8034968c838dc6a16b308c747aab7ee3c7c
matt <matt@mattvenn.net>
2015-04-25 23:14:05 +0000

    fixed player.setPos

--- teleport/teleport.py
+++ teleport/teleport.py
@@ -7,8 +7,12 @@ mc = minecraft.Minecraft.create()
 while True:
 	if pibrella.button.read() == 1:
 		mc.postToChat("teleporting!")
+
 		# pick a random spot
 		x = random.randint(-60,60)
 		y = random.randint(0,60)
 		z = random.randint(-60,60)
-		mc.setPos(x, y, z)
+		mc.player.setPos(x, y, z)
+		
+		# wait for button to be released
+		time.sleep(1)
