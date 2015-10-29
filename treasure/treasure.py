81ab3c68921dad7137639e0b23d88e39feb9d1ef
matt <matt@mattvenn.net>
2015-10-29 12:01:46 +0000

    wiki link

---
+++ treasure/treasure.py
@@ -0,0 +1,24 @@
+import mcpi.minecraft as minecraft
+import mcpi.block as block
+import time
+import random
+from mcpi.vec3 import Vec3
+from math import sqrt, pow
+
+mc = minecraft.Minecraft.create()
+
+# pick a random spot
+x = random.randint(-60,60)
+y = random.randint(0,60)
+z = random.randint(-60,60)
+destination = Vec3(x, y, z)
+
+while True:
+	playpos = mc.player.getTilePos()
+	
+	diff = playpos - destination
+	# work out how far away we are
+	# http://math.stackexchange.com/questions/42640/calculate-distance-in-3d-space
+	dist = sqrt(pow(diff.x, 2) + pow(diff.y, 2) + pow(diff.z, 2))
+	mc.postToChat(dist)
+	time.sleep(1)
