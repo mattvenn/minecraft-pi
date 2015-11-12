2068d844d519a60f72cde05abd5c5003b5094784
matt venn <matt@mattvenn.net>
2015-11-12 12:25:58 +0000

    works with pibrella

--- datalogger/datalogger.py
+++ datalogger/datalogger.py
@@ -1,6 +1,7 @@
 import mcpi.minecraft as minecraft
 import mcpi.block as block
 import time
+import pibrella
 
 mc = minecraft.Minecraft.create()
 import random
@@ -11,10 +12,19 @@ mc.setBlocks(-60,0,-60,60,50,60,block.AIR.id)
 # go there
 mc.player.setPos(5,0,0)
 
+data = 0
+
+def button_pressed(pin):
+    global data
+    data = data + 1
+
+pibrella.button.pressed(button_pressed)
+
 x = 0
 while True:
-	y = random.randint(1, 6)
-	x = x + 1
-	mc.setBlocks(x, 0, 0, x, y, 0, block.WOOL.id, 3)
+    print(data)
+    x = x + 1
+    mc.setBlocks(x, 0, 0, x, data, 0, block.WOOL.id, 3)
+    data = 0
 	
-	time.sleep(1)
+    time.sleep(1)
