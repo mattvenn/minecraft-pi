0f89c2f0fb83f877c2d79253b2bc62a92aafdf22
matt venn <matt@mattvenn.net>
2016-02-18 21:42:14 +0100

    ordering and loops

--- handout/prep.sh
+++ handout/prep.sh
@@ -1,34 +1,20 @@
 rm *md *jpg *png
 cp ../documentation/*png .
 
+cp ../intro/* .
+cp ../traffic_lights/* .
+cp ../teleport/* .
 cp ../trap/* .
 cp ../photobooth/* .
 cp ../big_buildings/* .
-cp ../traffic_lights/* .
 cp ../pixel_art/* .
-cp ../teleport/* .
 cp ../treasure/* .
 
 echo '% Python and Minecraft' > handout.md
 echo '% CPD for Teachers' >> handout.md
 echo '% ' >> handout.md
 
-cat big_buildings.md  >> handout.md
-echo -e '\\newpage\n' >> handout.md
-
-cat traffic.md  >> handout.md
-echo -e '\\newpage\n' >> handout.md
-
-cat trap.md >> handout.md
-echo -e '\\newpage\n' >> handout.md
-
-cat photobooth.md >> handout.md
-echo -e '\\newpage\n' >> handout.md
-
-cat pixel_art.md  >> handout.md
-echo -e '\\newpage\n' >> handout.md
-
-cat teleport.md  >> handout.md
-echo -e '\\newpage\n' >> handout.md
-
-cat treasure.md >> handout.md
+for file in intro traffic teleport trap photobooth big_buildings pixel_art treasure; do
+    cat $file.md  >> handout.md
+    echo -e '\\newpage\n' >> handout.md
+done
